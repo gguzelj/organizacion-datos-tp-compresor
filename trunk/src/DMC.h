a3844eeeeda4fa15a02e8b32fc1d0925671d602f
german.guzelj@gmail.com <german.guzelj@gmail.com@3450f063-c5c2-7424-1d91-abe6ec673335>
2014-04-23 04:35:45 +0000

    Cambios en el modelo del DMC

--- trunk/src/DMC.h
+++ trunk/src/DMC.h
@@ -1,15 +1,19 @@
 #ifndef DMC_H
 #define DMC_H
 
-//&---------------------------------------------------------------------------&
-//& Clase DMC: Dynamic Markov Compression
-//& Clase encargada de representar la maquina
-//& de estados finitos del metodo de Markov
-//&---------------------------------------------------------------------------&
+#include <list>
+#include "utils/constantes.h"
+#include "Estado.h"
+
+/**
+//---------------------------------------------------------------------------&
+// Clase:       DCM
+// Descripcion: Dynamic Markov Compression. Clase encargada de representar
+//              la maquina de estados finitos del metodo de Markov
+//---------------------------------------------------------------------------&
+*/
 class DMC
 {
-    int M1;
-    int M2;
 
 //&---------------------------------------------------------------------------&
 //& P U B L I C
@@ -20,15 +24,15 @@ class DMC
         virtual ~DMC();
 
 //&---------------------------------------------------------------------------&
-//& P R O T E C T E D
-//&---------------------------------------------------------------------------&
-    protected:
-
-
-//&---------------------------------------------------------------------------&
 //& P R I V A T E
 //&---------------------------------------------------------------------------&
     private:
-};
 
+        int                 M1;
+        int                 M2;
+        Estado*             estadoActual;
+        std::list<Estado*>  estados;
+
+
+};
 #endif // DMC_H
---
+++ trunk/src/utils/constantes.h
@@ -0,0 +1,25 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+/**
+//---------------------------------------------------------------------------&
+// Descripcion:
+// Archivo con constantes y tipos de datos utilizados en el compresor
+//---------------------------------------------------------------------------&
+*/
+
+/*  Tipos de datos  */
+typedef unsigned char Byte;
+typedef unsigned char Direccion;
+typedef unsigned long Nombre;
+typedef int Frecuencias[4];
+
+/*  Errores */
+
+/*  Constantes  */
+const Direccion BITS_00 = 0;
+const Direccion BITS_01 = 1;
+const Direccion BITS_10 = 2;
+const Direccion BITS_11 = 3;
+
+#endif // CONSTANTES_H_INCLUDED
