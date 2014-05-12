fc19c5cf212b05668d38f0c076c0ac7e40f0502b
TranceThisLife <TranceThisLife@3450f063-c5c2-7424-1d91-abe6ec673335>
2014-05-12 04:07:30 +0000

    Intento subir lo antes explicado

---
+++ src/CompresorAritmetico.h
@@ -0,0 +1,33 @@
+#ifndef COMPRESORARITMETICO_H
+#define COMPRESORARITMETICO_H
+#define TECHO 1000000000
+#define PISO 0
+#include <list>
+#include "Intervalo.h"
+
+class CompresorAritmetico
+{
+    public:
+
+    /**Metodos Publicos*/
+        CompresorAritmetico(int cantidadDeCaracteresDistintos);
+        void comprimirCaracterConFrecuencias(Caracter* caracterAcomprimir, std::list<Caracter*>& frecuenciasDeCaracteres);
+        unsigned long int getValorDeCompresionFinal();
+        virtual ~CompresorAritmetico();
+
+    protected:
+    private:
+
+    /**Atributos*/
+        /*std::list <Caracter> caracteresAcomprimir;*/
+        std::list <Intervalo*> intervalos;
+        unsigned long int techo;
+        unsigned long int piso;
+        int cantidadDeCaracteresVistos;
+
+    /**Metodos Privados*/
+        void eliminarIntervalos();
+
+};
+
+#endif // COMPRESORARITMETICO_H
---
+++ src/Intervalo.cpp
@@ -0,0 +1,38 @@
+#include "Intervalo.h"
+
+Intervalo::Intervalo(unsigned long int* unPiso, unsigned long int* unTecho, Caracter* unCaracter)
+{
+    piso = *unPiso;
+
+    techo = *unTecho;
+
+    caracter = unCaracter;
+}
+
+unsigned long int Intervalo::getPiso()
+{
+
+    return piso;
+
+}
+
+unsigned long int Intervalo::getTecho()
+{
+
+    return techo;
+
+}
+
+Caracter* Intervalo::getCaracter()
+{
+
+    return caracter;
+
+}
+
+Intervalo::~Intervalo()
+{
+
+    delete caracter;
+
+}
